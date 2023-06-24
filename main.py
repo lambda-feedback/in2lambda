7c9e34f168e58176b2e05857de7a9683156c5d72
Peter Johnson <peterbjohnson@Peters-MacBook-Pro.local>
2023-06-24 01:35:15 +0100

    JSON/zip output for masterContent (only)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import pypandoc
+import zipfile
 
 # Import a latex file with pandoc
 input_data = pypandoc.convert_file('example.tex', 'json')
@@ -40,28 +41,42 @@ def content_extractor(par):
     else:
         return ''
 
-# Main code
-def main():
+def converter(template):
     # Loop through the blocks in the content until an 'orderedlist' is found. This is specific to the way the example latex file is created, using \begin{enumerate}
     for block in parsed_data['blocks']:
         if block['t']=='OrderedList':
             block_lists = block['c'][1:]                                                    # Ignore [0] as a header, go from [1] onwards
             for block_list in block_lists:
                 for i,question in enumerate(block_list):                                    # The JSON structure has another layer of nesting
-                    print('Q'+str(i+1)+'\n\n')                                              # Printing for demo purposes only
+                    #print('Q'+str(i+1)+'\n\n')                                              # Printing for demo purposes only
+                    lambda_output=template
+                    output = ''
                     for j,par in enumerate(question):                                       # Question content is composed of multiple elements (paragraphs, maths, lists, etc.)
                         if par['t']=='Para':                                                # Normal paragraphs i.e. for master content                               
-                            output = content_extractor(par)                                 # 
-                            print(output)                                                   # Write to JSON master content here
+                            output += content_extractor(par)  # Write to JSON master content here
                         elif par['t']=='OrderedList':
                             par_lists = par['c'][1:]                                        # List in a list for parts of a questions (part (a), (b), etc.)
                             for par_list in par_lists:
                                 for k,part in enumerate(par_list):
-                                    print('Q'+str(i+1)+'('+chr(k+ord('a'))+')\n\n')
-                                    output = content_extractor(part[0])
-                                    print(output)                                           # Write to JSON part content here
-                                    print('\n\n')
+                                    # This part does convert, but isn't written to the JSON yet (unfinished work)
+                                    print()
+                                    #print('Q'+str(i+1)+'('+chr(k+ord('a'))+')\n\n')
+                                    #This is the part to change: template['parts'][j]['content']['blocks'][0]['data']='bvlah'
+                                    #output += content_extractor(part[0])
+                                    #print(output)                                           # Write to JSON part content here
+                                    #print('\n\n')
+                    lambda_output['masterContent']['blocks'][0]['data']=output
+                    filename='Q'+str(i+1)
+                    with open(filename+'.json','w') as file:
+                        json.dump(lambda_output,file) 
+                    with zipfile.ZipFile(filename+'.zip', "w") as zipf:
+                        zipf.write(filename+'.json', arcname=filename+'.json')
 
+# Main code
+def main():
+    with open("minimal_template.json", "r") as file:
+        template = json.load(file)
+    converter(template)
 
 if __name__ == '__main__':
     main()
