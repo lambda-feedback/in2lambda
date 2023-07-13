dd15ff270ecd2bfca0797fc9d910479c43264768
lewismoss <138524447+lewismoss@users.noreply.github.com>
2023-07-13 15:07:29 +0100

    katex_convert: Added logging and fixed some dictionary entries
    Added a log using the 'logging' library. The log writes to a file which entries in the dictionaries have been used. Future iterations could include the line number, or even append the original input with comments showing exactly what was removed.
    Also updated some dictionary entires which had incorrect syntax.

--- tex2lambda/katex_convert/katex_convert.py
+++ tex2lambda/katex_convert/katex_convert.py
@@ -5,7 +5,24 @@ Created on Wed Jul  5 19:13:28 2023
 @author: Matthew Howarth
 """
 import re
+import logging
 
+# Create a logger object with a name and a level
+logger = logging.getLogger("log")
+logger.setLevel(logging.INFO)
+
+# Create a file handler to write the messages to a file
+file_handler = logging.FileHandler("log", mode='w') # Clears log with every run
+file_handler.setLevel(logging.INFO)
+
+# Create a formatter to format the messages
+formatter = logging.Formatter("%(message)s")
+
+# Add the formatter to the file handler
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
 
 def latex_to_katex(latex_string: str) -> str:
     # Replace incompatible LaTeX functions with KaTeX compatible equivalents
@@ -14,7 +31,7 @@ def latex_to_katex(latex_string: str) -> str:
 
 
 def delete_functions(latex_string: str) -> str:
-    file_path = "tex2lambda/katex_convert/delete_list.txt"
+    file_path = "/Users/lewis/Documents/GitHub/content-conversion/tex2lambda/katex_convert/delete_list.txt"
     delete_list = []
 
     with open(file_path, "r") as file:
@@ -29,6 +46,10 @@ def delete_functions(latex_string: str) -> str:
             if match:
                 start_index=match.start()
                 end_index=match.end()
+
+                # Log the deletion of the function
+                logger.info(f"Deleted {latex_string[start_index:end_index]}")
+
                 if latex_string[end_index]=="{":
                     latex_string = brace_remover(latex_string, end_index)
                 latex_string = latex_string[:start_index-1] + latex_string[end_index:]
@@ -37,7 +58,7 @@ def delete_functions(latex_string: str) -> str:
     return latex_string
 
 def replace_functions(latex_string: str) -> str:
-    file_path = "tex2lambda/katex_convert/replace_list.txt"  # Replace with your file path
+    file_path = "/Users/lewis/Documents/GitHub/content-conversion/tex2lambda/katex_convert/replace_list.txt"  # Replace with your file path
 
     replacement_dict = {}  # Dictionary to store the formatted values
 
@@ -53,9 +74,15 @@ def replace_functions(latex_string: str) -> str:
             # Adding the formatted key-value pair to the dictionary
             replacement_dict[key] = value
 
+    logger.info("")
+
     # replace the incompatible functions with their KaTeX equivalents using re.sub
     for old, new in replacement_dict.items():
-        latex_string = re.sub(old, new, latex_string)
+        while re.search(old, latex_string):
+            match=re.search(old, latex_string)
+            if match:
+                logger.info(f"Replaced {old} with {new}")
+                latex_string = re.sub(old, new, latex_string)       
     return latex_string
 
 def brace_remover(latex_string, brace_start_index):
@@ -78,7 +105,12 @@ def brace_remover(latex_string, brace_start_index):
 
 if __name__ == "__main__":
     latex_input = r"""
-
+    2x^2
+    \newcounter{sdjn}
+    \mit
+    \scr
+    \eqref
+    3x
 """
 
     katex_output = latex_to_katex(latex_input)
