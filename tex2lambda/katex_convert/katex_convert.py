b520b390cf516aea4eb191dd7f005c27827fa790
harens <12570877+harens@users.noreply.github.com>
2023-07-19 13:37:23 +0100

    katex_convert: Remove hardcoded paths
    Also run black/isort formatters

--- tex2lambda/katex_convert/katex_convert.py
+++ tex2lambda/katex_convert/katex_convert.py
@@ -4,15 +4,16 @@ Created on Wed Jul  5 19:13:28 2023
 
 @author: Matthew Howarth
 """
-import re
 import logging
+import re
+from pathlib import Path
 
 # Create a logger object with a name and a level
 logger = logging.getLogger("log")
 logger.setLevel(logging.INFO)
 
 # Create a file handler to write the messages to a file
-file_handler = logging.FileHandler("log", mode='w') # Clears log with every run
+file_handler = logging.FileHandler("log", mode="w")  # Clears log with every run
 file_handler.setLevel(logging.INFO)
 
 # Create a formatter to format the messages
@@ -24,6 +25,7 @@ file_handler.setFormatter(formatter)
 # Add the file handler to the logger
 logger.addHandler(file_handler)
 
+
 def latex_to_katex(latex_string: str) -> str:
     # Replace incompatible LaTeX functions with KaTeX compatible equivalents
     katex_string = replace_functions(delete_functions(latex_string))
@@ -31,38 +33,35 @@ def latex_to_katex(latex_string: str) -> str:
 
 
 def delete_functions(latex_string: str) -> str:
-    file_path = "/Users/lewis/Documents/GitHub/content-conversion/tex2lambda/katex_convert/delete_list.txt"
     delete_list = []
 
-    with open(file_path, "r") as file:
+    with open(Path(__file__).with_name("delete_list.txt"), "r") as file:
         for line in file:
             line = line.strip()
-            if line:  
+            if line:
                 raw_line = r"{}".format(line)
                 delete_list.append(raw_line)
     for item in delete_list:
         while re.search(item, latex_string):
-            match=re.search(item, latex_string)
+            match = re.search(item, latex_string)
             if match:
-                start_index=match.start()
-                end_index=match.end()
+                start_index = match.start()
+                end_index = match.end()
 
                 # Log the deletion of the function
                 logger.info(f"Deleted {latex_string[start_index:end_index]}")
 
-                if latex_string[end_index]=="{":
+                if latex_string[end_index] == "{":
                     latex_string = brace_remover(latex_string, end_index)
                 latex_string = latex_string[:start_index] + latex_string[end_index:]
 
-
     return latex_string
 
-def replace_functions(latex_string: str) -> str:
-    file_path = "/Users/lewis/Documents/GitHub/content-conversion/tex2lambda/katex_convert/replace_list.txt"  # Replace with your file path
 
+def replace_functions(latex_string: str) -> str:
     replacement_dict = {}  # Dictionary to store the formatted values
 
-    with open(file_path, "r") as file:
+    with open(Path(__file__).with_name("replace_list.txt"), "r") as file:
         for line in file:
             # Removing leading/trailing whitespaces, newlines, and commas
             line = line.strip().replace(",", "")
@@ -77,8 +76,8 @@ def replace_functions(latex_string: str) -> str:
             match = pattern.search(value)
 
             if match:
-                key = key + r'(?![a-zA-Z])'
-    
+                key = key + r"(?![a-zA-Z])"
+
             replacement_dict[key] = value
 
     logger.info("")
@@ -86,10 +85,10 @@ def replace_functions(latex_string: str) -> str:
     # replace the incompatible functions with their KaTeX equivalents using re.sub
     for old, new in replacement_dict.items():
         while re.search(old, latex_string):
-            match=re.search(old, latex_string)
+            match = re.search(old, latex_string)
             if match:
                 logger.info(f"Replaced {old} with {new}")
-                latex_string = re.sub(old, new, latex_string)       
+                latex_string = re.sub(old, new, latex_string)
     return latex_string
 
 
@@ -105,11 +104,12 @@ def brace_remover(latex_string: str, brace_start_index: int) -> str:
             case "}":
                 level_count -= 1
 
-    latex_string = latex_string[:brace_start_index] + latex_string[brace_start_index+1:]
-    latex_string = latex_string[:index_count-1] + latex_string[index_count:]
+    latex_string = (
+        latex_string[:brace_start_index] + latex_string[brace_start_index + 1 :]
+    )
+    latex_string = latex_string[: index_count - 1] + latex_string[index_count:]
     return latex_string
 
-    
 
 if __name__ == "__main__":
     latex_input = r"""
