c2c1ff996fd2f9a07b2fe54cdae3f66144978250
harens <12570877+harens@users.noreply.github.com>
2023-07-19 13:37:02 +0100

    json_convert: Run black/isort formatters

--- tex2lambda/json_convert/json_convert.py
+++ tex2lambda/json_convert/json_convert.py
@@ -1,10 +1,11 @@
 import json
 import os
-import zipfile
+import shutil
 from copy import deepcopy
 from pathlib import Path
-import shutil
 from typing import Any
+
+from tex2lambda.question import Question
 
 MINIMAL_TEMPLATE = "minimal_template.json"
 
@@ -13,8 +14,6 @@ def converter(
     template: dict[str, Any], ListQuestions: list[Question], output_dir: str
 ) -> None:
     # Create output by copying template
-    
-   
 
     for i in range(len(ListQuestions)):
         output = deepcopy(template)
@@ -23,7 +22,7 @@ def converter(
         if ListQuestions[i]["Title"] != "":
             output["title"] = ListQuestions[i]["Title"]
         else:
-            output["title"] = "Question " + str(i+1)
+            output["title"] = "Question " + str(i + 1)
 
         # add main text to the question file
         output["masterContent"]["blocks"][0]["data"] = ListQuestions[i]["MainText"]
@@ -45,31 +44,30 @@ def converter(
             ] = ListQuestions[i]["Parts"][j]["Answer"]
 
         # Output file)
-        filename = "Question " + str(i+1)
+        filename = "Question " + str(i + 1)
 
-        #create directory to put the questions
+        # create directory to put the questions
         os.makedirs(output_dir, exist_ok=True)
-        output_question=os.path.join(output_dir,filename)
-        os.makedirs(output_question,exist_ok=True)
+        output_question = os.path.join(output_dir, filename)
+        os.makedirs(output_question, exist_ok=True)
 
-        #create directory to put image
-        output_image=os.path.join(output_question,'media')
-        os.makedirs(output_image,exist_ok=True)
-      
+        # create directory to put image
+        output_image = os.path.join(output_question, "media")
+        os.makedirs(output_image, exist_ok=True)
 
-        #write questions into directory
+        # write questions into directory
         with open(f"{output_question}/{filename}.json", "w") as file:
             json.dump(output, file)
 
-        #write image into directory 
-        for k in range(len(ListQuestions[i]['Images'])):
-            image_path=os.path.abspath(ListQuestions[i]['Images'][k]) #converts computer path into python path
-            shutil.copy(image_path,output_image) #copies image into the directory
-    
-        #output zip file in destination folder
-        shutil.make_archive(output_question, 'zip', output_question) 
-        
-      
+        # write image into directory
+        for k in range(len(ListQuestions[i]["Images"])):
+            image_path = os.path.abspath(
+                ListQuestions[i]["Images"][k]
+            )  # converts computer path into python path
+            shutil.copy(image_path, output_image)  # copies image into the directory
+
+        # output zip file in destination folder
+        shutil.make_archive(output_question, "zip", output_question)
 
 
 def main(questions: list[Question], output_dir: str) -> None:
