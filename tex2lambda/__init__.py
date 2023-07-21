3a1e45ce1bc0ff6270ecbd485fc480459ee2ba1e
harens <12570877+harens@users.noreply.github.com>
2023-07-21 13:07:20 +0100

    deps: add beartype
    See https://github.com/beartype/beartype
    
    Used for quick runtime type checking

---
+++ tex2lambda/__init__.py
@@ -0,0 +1,5 @@
+"""Python library and CLT for converting LaTeX documents into Lambda Feedback compatible JSON/ZIP files."""
+
+from beartype.claw import beartype_this_package
+
+beartype_this_package()
