f1ec6eca06ea0523bfa88c05567fb244f1114be0
TeeVahReeeeeen <tvarine@googlemail.com>
2024-07-08 13:38:57 +0100

    removed local running thingy

--- in2lambda/main.py
+++ in2lambda/main.py
@@ -1,8 +1,8 @@
 """The main input for in2lambda, defining both the CLT and main library function."""
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
  
 
 import importlib
