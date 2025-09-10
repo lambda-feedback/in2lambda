9abc75336e3853be684bbe44631bc8096fd092e8
HarrySu123 <147165239+HarrySu123@users.noreply.github.com>
2025-09-10 10:59:23 +0100

    Added ordered question inserting feature and more configuration options for set (#19)
    * added ordering to the questions
    
    * renamed all use of module with set
    
    * included minimal json template for set in json converter
    
    * implemented sorted imports
    
    * refactoring and now allows setting name and description for the set
    
    * updated yml file
    
    * removed virtualenv from the test.yml file
    
    * used a safer version of requests
    
    * corrected formatted and sorted imports in PartsSepSol filter
    
    * ran isort . and black . to correctly format everything
    
    * further fixing of formatting in function summaries and description
    
    * removed blank line after function docstring in set.py
    
    * correected tests to match new results
    
    * added attributes for modifable visiblity for finalanswers , wokred solution and structured tutorials
    
    * added visibility settings to API
    
    * corretly formatted and sorted imports in set.py
    
    * corrected formatting on import in set.py
    
    * resolved isort and black conflict
    
    * Added missing docstrings for methods in visibility_status
    
    * added module-level docstring for visibility_status
    
    * filename generation now handles special characters correctly

--- in2lambda/filters/PartPartSolSol/filter.py
+++ in2lambda/filters/PartPartSolSol/filter.py
@@ -6,7 +6,7 @@ from typing import Optional
 
 import panflute as pf
 
-from in2lambda.api.module import Module
+from in2lambda.api.set import Set
 from in2lambda.filters.markdown import filter
 
 
@@ -14,7 +14,7 @@ from in2lambda.filters.markdown import filter
 def pandoc_filter(
     elem: pf.Element,
     doc: pf.elements.Doc,
-    module: Module,
+    set: Set,
     parsing_answers: bool,
 ) -> Optional[pf.Str]:
     """A Pandoc filter that parses and translates various TeX elements.
@@ -23,7 +23,7 @@ def pandoc_filter(
         elem: The current TeX element being processed. This could be a paragraph,
             ordered list, etc.
         doc: A Pandoc document container - essentially the Pandoc AST.
-        module: The Python API that is used to store the result after processing
+        set: The Python API that is used to store the result after processing
             the TeX file.
         parsing_answers: Whether an answers-only document is currently being parsed.
 
@@ -46,10 +46,10 @@ def pandoc_filter(
 
     # Solutions are in a Div
     if isinstance(elem, pf.Div):
-        module.add_question(main_text="\n".join(pandoc_filter.question))
+        set.add_question(main_text="\n".join(pandoc_filter.question))
         if hasattr(pandoc_filter, "parts"):
             for part in pandoc_filter.parts:
-                module.current_question.add_part_text(part)
+                set.current_question.add_part_text(part)
         pandoc_filter.question = []
         pandoc_filter.parts = []
 
@@ -58,8 +58,8 @@ def pandoc_filter(
         match type(first_answer_part := elem.content[0]):
             case pf.OrderedList:
                 for item in first_answer_part.content:
-                    module.current_question.add_solution(pf.stringify(item))
+                    set.current_question.add_solution(pf.stringify(item))
             case pf.Para:
-                module.current_question.add_solution(pf.stringify(elem))
+                set.current_question.add_solution(pf.stringify(elem))
 
     return None
